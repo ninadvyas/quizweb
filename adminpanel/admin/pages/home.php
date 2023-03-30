03dd5338734cb72050072c801f9f1c35711e0db2
Ninad Vyas <115214615+NinadVyas@users.noreply.github.com>
2023-03-30 23:13:06 +0530

    Initial commit

---
+++ adminpanel/admin/pages/home.php
@@ -0,0 +1,142 @@
+
+
+<div class="app-main__outer">
+    <div id="refreshData">
+    <div class="app-main__inner">
+          <div class="row">
+                <div class="col-md-6 col-xl-4">
+                    <div class="card mb-3 widget-content bg-midnight-bloom">
+                        <div class="widget-content-wrapper text-white">
+                            <div class="widget-content-left">
+                                <div class="widget-heading">Total Course</div>
+                                <div class="widget-subheading" style="color:transparent;">.</div>
+                            </div>
+                            <div class="widget-content-right">
+                                <div class="widget-numbers text-white">
+                                    <span><?php echo $totalCourse = $selCourse['totCourse']; ?></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-xl-4">
+                    <div class="card mb-3 widget-content bg-arielle-smile">
+                        <div class="widget-content-wrapper text-white">
+                            <div class="widget-content-left">
+                                <div class="widget-heading">Total Exam</div>
+                                <div class="widget-subheading" style="color:transparent;">.</div>
+                            </div>
+                            <div class="widget-content-right">
+                                <div class="widget-numbers text-white">
+                                    <span><?php echo $totalCourse = $selExam['totExam']; ?></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-xl-4">
+                    <div class="card mb-3 widget-content bg-grow-early">
+                        <div class="widget-content-wrapper text-white">
+                            <div class="widget-content-left">
+                                <div class="widget-heading">Total Examinee</div>
+                                <div class="widget-subheading" style="color:transparent;">.</div>
+                            </div>
+                            <div class="widget-content-right">
+                                <div class="widget-numbers text-white"><span>46%</span></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="d-xl-none d-lg-block col-md-6 col-xl-4">
+                    <div class="card mb-3 widget-content bg-premium-dark">
+                        <div class="widget-content-wrapper text-white">
+                            <div class="widget-content-left">
+                                <div class="widget-heading">Products Sold</div>
+                                <div class="widget-subheading">Revenue streams</div>
+                            </div>
+                            <div class="widget-content-right">
+                                <div class="widget-numbers text-warning"><span>$14M</span></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    
+            <!-- <div class="row">
+                <div class="col-md-6 col-xl-4">
+                    <div class="card mb-3 widget-content">
+                        <div class="widget-content-outer">
+                            <div class="widget-content-wrapper">
+                                <div class="widget-content-left">
+                                    <div class="widget-heading">Total Orders</div>
+                                    <div class="widget-subheading">Last year expenses</div>
+                                </div>
+                                <div class="widget-content-right">
+                                    <div class="widget-numbers text-success">1896</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-xl-4">
+                    <div class="card mb-3 widget-content">
+                        <div class="widget-content-outer">
+                            <div class="widget-content-wrapper">
+                                <div class="widget-content-left">
+                                    <div class="widget-heading">Products Sold</div>
+                                    <div class="widget-subheading">Revenue streams</div>
+                                </div>
+                                <div class="widget-content-right">
+                                    <div class="widget-numbers text-warning">$3M</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-xl-4">
+                    <div class="card mb-3 widget-content">
+                        <div class="widget-content-outer">
+                            <div class="widget-content-wrapper">
+                                <div class="widget-content-left">
+                                    <div class="widget-heading">Followers</div>
+                                    <div class="widget-subheading">People Interested</div>
+                                </div>
+                                <div class="widget-content-right">
+                                    <div class="widget-numbers text-danger">45,9%</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="d-xl-none d-lg-block col-md-6 col-xl-4">
+                    <div class="card mb-3 widget-content">
+                        <div class="widget-content-outer">
+                            <div class="widget-content-wrapper">
+                                <div class="widget-content-left">
+                                    <div class="widget-heading">Income</div>
+                                    <div class="widget-subheading">Expected totals</div>
+                                </div>
+                                <div class="widget-content-right">
+                                    <div class="widget-numbers text-focus">$147</div>
+                                </div>
+                            </div>
+                            <div class="widget-progress-wrapper">
+                                <div class="progress-bar-sm progress-bar-animated-alt progress">
+                                    <div class="progress-bar bg-info" role="progressbar" aria-valuenow="54" aria-valuemin="0" aria-valuemax="100" style="width: 54%;"></div>
+                                </div>
+                                <div class="progress-sub-label">
+                                    <div class="sub-label-left">Expenses</div>
+                                    <div class="sub-label-right">100%</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div> -->
+
+            <!-- <?php include("includes/graph.php"); ?> -->
+      
+        
+        </div>
+         
+    </div>
