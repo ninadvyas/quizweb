3b88cd962f0c0dbe44a782ac7b0a0bb281935d26
Ninad Vyas <115214615+NinadVyas@users.noreply.github.com>
2023-03-31 00:01:04 +0530

    Update index.php

--- adminpanel/admin/login-ui/index.php
+++ adminpanel/admin/login-ui/index.php
@@ -22,7 +22,7 @@
 			<div class="wrap-login100">
 				<div class="login100-form-title" style="background-image: url(login-ui/images/Hey.png);">
 					<span class="login100-form-title-1">
-						Sign In
+						Admin
 					</span>
 				</div>
 
